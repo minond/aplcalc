55781f25112a0cf76c02b43667e5d7e7fdbd3312
Marcos Minond <minond.marcos@gmail.com>
2019-06-20 17:55:49 -0600

    repl commands

--- main.go
+++ main.go
@@ -8,14 +8,23 @@ func main() {
 		output: os.Stdout,
 	}
 
+loop:
 	for {
 		r.write("? ")
 		input, _ := r.read()
-		expr, err := parse(input)
-		if err != nil {
-			r.write("syntax error: %v\n\n", err)
-		} else {
-			r.write("%s\n\n", expr)
+		switch input {
+		case "":
+			continue
+		case "exit":
+			break loop
+		default:
+			expr, err := parse(input)
+			if err != nil {
+				r.write("tokens: %s\n", tokenize(input))
+				r.write("syntax error: %v\n\n", err)
+			} else {
+				r.write("%s\n\n", expr.Stringify(0))
+			}
 		}
 	}
 }
