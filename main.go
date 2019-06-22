8c9baa93181ed6ea12b28d657daa9f5bad0d8d98
Marcos Minond <minond.marcos@gmail.com>
2019-06-22 15:37:06 -0600

    repl package

--- main.go
+++ main.go
@@ -4,45 +4,46 @@ import (
 	"os"
 
 	"github.com/minond/calc/parser"
+	"github.com/minond/calc/repl"
 	"github.com/minond/calc/value"
 )
 
 func main() {
-	var debug bool
+	debug := false
+	running := true
 	env := value.NewEnvironment()
 
-	r := repl{
-		input:  os.Stdin,
-		output: os.Stdout,
+	r := repl.Repl{
+		Input:  os.Stdin,
+		Output: os.Stdout,
 	}
 
-loop:
-	for {
-		r.write("? ")
-		input, _ := r.read()
+	for running {
+		r.Write("? ")
+		input, _ := r.Read()
 		switch input {
 		case "":
 			continue
 		case "exit":
-			break loop
+			running = false
 		case "debug":
 			debug = !debug
 		default:
 			expr, err := parser.Parse(input)
 			if err != nil {
-				r.write("syntax error: %v\n\n", err)
+				r.Write("syntax error: %v\n\n", err)
 				continue
 			} else if debug {
-				r.write("%s\n\n", expr.Stringify(0))
+				r.Write("%s\n\n", expr.Stringify(0))
 				continue
 			}
 
 			val, err := value.Eval(env, expr)
 			if err != nil {
-				r.write("error: %v\n\n", err)
+				r.Write("error: %v\n\n", err)
 			} else {
 				env.SetVal("_", val)
-				r.write("= %s\n\n", val.Stringify())
+				r.Write("= %s\n\n", val.Stringify())
 			}
 		}
 	}
---
+++ repl/repl.go
@@ -0,0 +1,27 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Repl struct {
+	Input  io.Reader
+	Output io.Writer
+}
+
+func (repl Repl) Read() (string, error) {
+	reader := bufio.NewReader(repl.Input)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(input), nil
+}
+
+func (repl Repl) Write(s string, a ...interface{}) error {
+	_, err := fmt.Fprintf(repl.Output, s, a...)
+	return err
+}
